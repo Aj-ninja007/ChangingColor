500a97b9ce399393d474481165dd6b0b9f9465ef
Prince mishra <113469532+Aj-ninja007@users.noreply.github.com>
2023-10-22 01:20:42 +0530

    Update stack.py

--- STACK/stack.py
+++ STACK/stack.py
@@ -1,1 +1,17 @@
 
+def evalpost(s):
+    stack=[]
+    for i in s:
+        if i not in ['^','*','/','+','-','(',')']:
+            stack.append(i)
+        else:
+            a=stack.pop()
+            b=stack.pop()
+            stack.append(f'({b}{i}{a})')
+    return stack[-1]
+t="ab+cd+*"
+print(evalpost(t))
+
+
+
+
